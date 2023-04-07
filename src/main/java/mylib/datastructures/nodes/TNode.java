5b6140c326ce5c0352650db28e70d6c4cb319cc8
Rimal01 <rimal.rizvi@ucalgary.ca>
2023-04-07 17:04:33 -0600

    Adding code for BST, and the test

--- src/main/java/mylib/datastructures/nodes/TNode.java
+++ src/main/java/mylib/datastructures/nodes/TNode.java
@@ -1,5 +1,79 @@
 package main.java.mylib.datastructures.nodes;
 
 public class TNode {
+    private int data;
+    private TNode left;
+    private TNode right;
+    private TNode parent;
+    private int balance;
     
+    // Default constructor
+    public TNode() {
+        this.data = 0;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.balance = 0;
+    }
+    
+    // Overload constructor
+    public TNode(int data, int balance, TNode parent, TNode left, TNode right) {
+        this.data = data;
+        this.balance = balance;
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+    }
+    
+    // Getters and Setters
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TNode left) {
+        this.left = left;
+    }
+
+    public TNode getRight() {
+        return right;
+    }
+
+    public void setRight(TNode right) {
+        this.right = right;
+    }
+
+    public TNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TNode parent) {
+        this.parent = parent;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
+    // toString method
+    public String toString() {
+        return Integer.toString(data);
+    }
+    
+    // print method
+    public void print() {
+        System.out.println("Data: " + data + ", Balance: " + balance);
+    }
 }
+
